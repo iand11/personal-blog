92a69aada91b1330d8d258ccc12b9f40be9ffd3d
iand11 <ian.driscoll.o@gmail.com>
2017-07-06 10:50:12 -0700

    Add functionality to latest post so it shows the 3 most recent posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,14 @@ class PostsController < ApplicationController
 
   def index
     @all_posts = Post.all
+    @recent_posts = Post.all.order('created_at DESC').take(3)
   end
 
 
   def show
     @post = Post.find(params[:id])
     @all_posts = Post.all
+    @recent_posts = Post.all.order('created_at DESC').take(3)
   end
 
 end
