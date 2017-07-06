92f405cd1e63a372a753548164102999c8138ffc
iand11 <ian.driscoll.o@gmail.com>
2017-07-06 15:52:42 -0700

    Add functionality to user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect_to root_path
+    else
+      @errors = ["Incorrect"]
+      render 'new'
+    end 
+  end
+
+  def destroy
+    session[:id] = nil
+    redirect_to root_path
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  def login(user)
+    session[:id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout 
+    session[:id] = nil
+  end
+
+  def authorize 
+    unless logged_in?
+      flash[:logged_in_error] = "You must be logged in."
+      redirect_to_new_session_path
+    end
+  end
 end
