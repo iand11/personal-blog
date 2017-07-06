1ecd7e1a5abf1e208612032478e6371d05264db6
iand11 <ian.driscoll.o@gmail.com>
2017-07-06 15:28:04 -0700

    Add create, update, and destroy for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,43 @@ class PostsController < ApplicationController
     @recent_posts = Post.all.order('created_at DESC').take(3)
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end 
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to root_path
+    else
+      render "edit"
+    end 
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy 
+    redirect_to root_path
+  end
+
+
+
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
