272a54ac8a3b5587da2b965a6e295375a196059c
iand11 <ian.driscoll.o@gmail.com>
2017-07-06 18:02:25 -0700

    Fix issue with post counter and arrows when post was deleted. Now using helper methods instead of post.id

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,26 @@
 module PostsHelper
+  
+  def all_current_posts
+    posts = []
+    @all_posts.each do |post|
+      posts << post
+    end 
+    posts
+  end
+
+  def current_post(x)
+    all_current_posts.index(x) + 1
+  end
+
+  def next_post(x)
+    a = all_current_posts.index(x)
+    b = all_current_posts[a+1]
+    b.id
+  end
+
+  def prev_post(x)
+    a = all_current_posts.index(x)
+    b = all_current_posts[a-1]
+    b.id
+  end
 end
